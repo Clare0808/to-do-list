dbf4739c8241e40d77774e9d8681faac3c43e29d
Clare <yuhsin099@gmail.com>
2025-06-11 15:21:41 +0800

    add error message

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="error-message">Please enter tasks !</div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage'
+}
+</script>
+
+<style scoped>
+.error-message {
+  color: #FF5151;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
